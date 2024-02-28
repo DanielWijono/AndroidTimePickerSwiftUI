2239cc55085b38224e712dcebc2fc5961f9ff820
Silverius Daniel Wijono <swijono@quadrant.io>
2024-02-28 10:06:23 +0700

    Initial Commit

---
+++ AndroidSwiftUITimePicker/AndroidSwiftUITimePickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AndroidSwiftUITimePickerApp.swift
+//  AndroidSwiftUITimePicker
+//
+//  Created by Silverius Daniel Wijono on 28/02/24.
+//
+
+import SwiftUI
+
+@main
+struct AndroidSwiftUITimePickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AndroidSwiftUITimePicker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AndroidSwiftUITimePicker
+//
+//  Created by Silverius Daniel Wijono on 28/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
